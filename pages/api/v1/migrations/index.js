19ca7b71734bc80aefb06052b2ac14ac584914df
Gama <80848637+gabrielpassosmartins@users.noreply.github.com>
2024-10-06 23:57:45 +0000

    endpoint de migrations + jest.config

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,36 @@
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+import database from "infra/database.js";
+
+export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+
+  const defaultMigrationsOptions = {
+    dbClient: dbClient,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+
+  if (request.method === "GET") {
+    const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
+    await dbClient.end();
+    response.status(200).json(pendingMigrations);
+  }
+
+  if (request.method === "POST") {
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationsOptions,
+      dryRun: false,
+    });
+
+    var statusResponse = 200;
+    if (migratedMigrations.length > 0) {
+      statusResponse = 201;
+    }
+    await dbClient.end();
+    response.status(statusResponse).json(migratedMigrations);
+  }
+}
